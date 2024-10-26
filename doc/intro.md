0f1a100ad54e4646883f0426f7c2434914a7379e
Nikola Vasic <nikola.vasic01@gmail.com>
2024-10-26 16:08:07 +0200

    Pocetak projetka

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cahelper
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cahelper/core_test.clj
@@ -0,0 +1,7 @@
+(ns cahelper.core-test
+  (:require [clojure.test :refer :all]
+            [cahelper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
