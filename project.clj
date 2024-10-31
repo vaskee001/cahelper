8da14eb8a7baf0dd90548d511a6ea509cf235ea7
Nikola Vasic <nikola.vasic01@gmail.com>
2024-10-31 08:37:10 +0100

    First function

--- project.clj
+++ project.clj
@@ -1,1 +1,3 @@
-(defproject cahelper "0.0.1-SNAPSHOT")
+(defproject cahelper "0.0.1-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.12.0"]]
+  :main cahelper.core)
---
+++ src/cahelper/core.clj
@@ -0,0 +1,3 @@
+(ns cahelper.core)
+(defn -main[& args]
+  (println "Kazem"))
