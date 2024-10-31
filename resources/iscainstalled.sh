ad66634db16a98df9d560c56d25cce14195ddcb7
Nikola Vasic <nikola.vasic01@gmail.com>
2024-10-31 14:48:33 +0100

    Changed script to ls

--- resources/iscainstalled.sh
+++ resources/iscainstalled.sh
@@ -4,7 +4,7 @@ command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
-if command_exists openvpn && command_exists easyrsa; then
+if command_exists openvpn; then
     echo "CA is present on the system."
 else
     echo "CA is not installed on the system."
